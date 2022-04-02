2310ccb781110a7788f0e6d1a7b561ac1e746dd7
Олег Матшин <89175955650@mail.ru>
2022-04-02 17:34:19 +0300

    Added method profile_list_photos_VK

--- Course_Project_Backup_py52_042022.py
+++ Course_Project_Backup_py52_042022.py
@@ -18,9 +18,9 @@ class VkGetPhoto:
             token = file.read().strip()    
         return token
 
-    # Get ID method of VK user with digits check.
+    # Get ID method of VK user with digits check and VK base check.
     def get_ID_VK():
-        """It is the get ID method of VK user with digits checking."""
+        """It is the get ID method of VK user with digits and VK base checking."""
         while True:
             owner_id = input('Please, input the ID VK user (attention: only digits!): ')
             if owner_id.isdigit():
@@ -63,6 +63,7 @@ class VkGetPhoto:
             print("Sorry! Different problems with this ID user!")
         else:
             pprint(get_profile_photos)
+        return get_profile_photos
 
     # Function converting date from sec to day-month-year.
     def convert_date(date_sec):
@@ -70,8 +71,34 @@ class VkGetPhoto:
         norm_date = datetime.fromtimestamp(date_sec).strftime("%d_%B_%Y")
         return norm_date  
 
+    # Function to create list of ID getting profile photos from the user account.
+    def profile_list_photos_VK(num_photos=5):
+        """This is the method creating list of ID getting profile photos from the user account."""
+        while True:
+            list_id_photos = []
+            count_photos = 0
+            try:
+                for item in VkGetPhoto.get_photos_VK():
+                    list_id_photos.append(item['id'])
+                    list_id_photos = list_id_photos[::-1]       # sorted from newest to oldest
+                    count_photos += 1
+            except UnboundLocalError:
+                print(f'It is not possible to create list photos for this user. Maybe it is closed or deleted profile.')
+                break
+            else:
+                if count_photos < num_photos:
+                    print(f'The user have only {count_photos} profile photos. It is less, then you need.')
+                elif count_photos == num_photos:
+                    print(f'The user have {count_photos} profile photos. You need {num_photos} too.')
+                elif count_photos > num_photos:
+                    print(f'The user have {count_photos} profile photos. You need {num_photos} The part of photos not will be include in list.')
+                list_id_photos = list_id_photos[:num_photos]
+                print(f'The default value is 5 photos. You requested {num_photos}. The list of photos is: {list_id_photos}.')
+                break
+        return list_id_photos
+
     # Function for the selection getting profile photos from the user ID account with the maximum sizes.
-    def selection_get_photos(num_photos=5):
+    def selection_get_photos():
         """It is the method for the selection getting profile photos from the user ID account with the maximum sizes"""
         
         return None
@@ -105,4 +132,7 @@ def progress_bar():
         time.sleep(1)
 
 
-VkGetPhoto.get_photos_VK()
+# testing part:
+# VkGetPhoto.get_photos_VK() - successfully
+# print(VkGetPhoto.convert_date(1562944607)) - successfully
+# VkGetPhoto.profile_list_photos_VK(1500) - successfully
